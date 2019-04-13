2dea113b4fa9169691e4b9dd9ad0e6da31d64023
danykivanov <danykivanov@gmail.com>
2019-04-13 11:43:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public void result()
+        {
+            int a, b;
+            int result;
+
+            Console.WriteLine("Enter a , b : ");
+          a = Convert.ToInt32(Console.ReadLine());
+          b = Convert.ToInt32(Console.ReadLine());
+
+            result = a / b;
+
+        }
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            int ggg;
+            a.result();
+        }
+    }
+}
